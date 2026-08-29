515c5112a8dd8cffaf692249751f1d37bad31ad6
Changhyo Shon <93428580+changhyoshon100@users.noreply.github.com>
2026-08-29 15:50:37 -0700

    Add: permutation-string - submission-17

---
+++ Data Structures & Algorithms/permutation-string/submission-17.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        hash1 = Counter(s1)
+        l = 0
+        r = len(s1)
+        hash2 = Counter(s2[l:r])
+
+        while r < len(s2):
+            if hash1 == hash2:
+                return True
+            else:
+                hash2[s2[l]] -= 1
+                l += 1
+
+                hash2[s2[r]] += 1
+                r += 1
+        return hash1 == hash2
