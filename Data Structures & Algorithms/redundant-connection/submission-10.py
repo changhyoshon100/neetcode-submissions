e986374a142a46a8788fbde73678409975aaadab
Changhyo Shon <93428580+changhyoshon100@users.noreply.github.com>
2026-09-09 11:25:13 -0700

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        mp = defaultdict(list)
+        cycle = set()
+        def dfs(crs, prev):
+            if crs in cycle:
+                return False
+            
+            cycle.add(crs)
+            for nei in mp[crs]:
+                if nei == prev:
+                    continue
+                if not dfs(nei, crs):
+                    return False
+            cycle.remove(crs)
+            return True
+
+
+        for crs, pre in edges:
+            mp[crs].append(pre)
+            mp[pre].append(crs)
+
+            if not dfs(crs, pre):
+                return [crs, pre]
